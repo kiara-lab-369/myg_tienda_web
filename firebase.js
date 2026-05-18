43e3c3e6b477c39e0d6617a87e9d566789ff86a8
kiara-lab-369 <63629728+kiara-lab-369@users.noreply.github.com>
2026-05-17 21:27:04 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,16 @@
+// Importar Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
 
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "myg-tienda.firebaseapp.com",
+  projectId: "myg-tienda",
+  storageBucket: "myg-tienda.firebasestorage.app",
+  messagingSenderId: "72865962233",
+  appId: "1:72865962233:web:ab24973b340bebed352a28"
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+
+console.log("Firebase conectado 🚀");
